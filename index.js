995e157c9a132cb29940b91ab4f1f5c7559710b0
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2024-06-06 12:54:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const monitorWebsite = require('./api/monitor.js');
 
-// Export the function for Vercel to use
-module.exports = monitorWebsite;
+// Handle incoming HTTP requests
+module.exports = async (req, res) => {
+    try {
+        await monitorWebsite();
+        res.status(200).send('Monitoring started successfully.');
+    } catch (error) {
+        console.error('Error starting monitoring:', error);
+        res.status(500).send('Internal server error.');
+    }
+};
